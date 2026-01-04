089f7b389ae29085514854f7632695b8b40c3655
Pen Peen <ikegami1022.h@gmail.com>
2026-01-04 21:37:38 +0900

    multiple argument

--- map.py
+++ map.py
@@ -12,3 +12,9 @@ print(values)   # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 # map()はイテレータを返すため、リスト化するにはlist()が必要
 mapped = list(map(lambda x: x * 10, values))
 print(mapped)   # [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+
+# 複数の引数
+names = ['Tom', 'Bob', 'Mike']
+ages = [18, 20, 22]
+
+print(list(map(lambda name, age: f"{name}:{age}", names, ages)))  # ['Tom:18', 'Bob:20', 'Mike:22']
